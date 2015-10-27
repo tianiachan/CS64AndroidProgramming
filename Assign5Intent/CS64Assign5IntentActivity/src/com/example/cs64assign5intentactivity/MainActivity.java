ecf35aef4c547aceb0974eabf28e7ce522f2d3dc
tianiachan <tiania.chan@gmail.com>
2015-10-27 16:46:12 -0700

    Reorganized and added code for Assign5:Practicing Intent

---
+++ Assign5Intent/CS64Assign5IntentActivity/src/com/example/cs64assign5intentactivity/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.cs64assign5intentactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	Button btnSecond, btnSearch;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btnSecond = (Button)findViewById(R.id.buttonSecond);
+		btnSearch = (Button)findViewById(R.id.buttonSearch);
+	}
+
+	public void secondScreen(View view)
+	{
+		Intent second = new Intent(this, SecondActivity.class);//create object
+		startActivity(second);//should launch second activity
+	}
+
+	public void searchScreen(View view)
+	{
+		Intent myIntent = new Intent();
+		myIntent.setAction(Intent.ACTION_VIEW);
+		myIntent.setData(Uri.parse("http://www.google.com"));
+		startActivity(myIntent);
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
